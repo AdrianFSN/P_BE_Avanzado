8ea9aafb10a18197bcae7928a7b8507127a1d668
Adrián S <adriansotes@gmail.com>
2024-05-26 18:02:59 +0200

    UniqueKeysRetriever throws an error if key doesn`t exists

--- routes/tags.js
+++ routes/tags.js
@@ -10,7 +10,7 @@ router.get("/", async function (req, res, next) {
   try {
     const tagsList = await UniqueKeysRetriever.getUniqueKeyValuesFromAPI("tag");
     //console.log('Esto es tagList', tagsList) // Me va a dar un objeto con results: lista de tags
-    //console.log('Esto es tagList.results', tagsList.results) //Me da ls lista incluída en results
+    //console.log('Esto es tagList.results', tagsList.results) //Me da ls lista incluida en results
     res.render("tags", { tags: tagsList.results });
   } catch (error) {
     next(error);
--- utils/UniqueKeysRetriever.js
+++ utils/UniqueKeysRetriever.js
@@ -5,6 +5,9 @@ class UniqueKeysRetriever {
   static async getUniqueKeyValuesFromAPI(key) {
     try {
       const retrievedList = await AdNopop.distinct(key);
+      if (retrievedList.length === 0) {
+        throw new Error(`Nothing found to display for key ${key}`);
+      }
       return { results: retrievedList };
     } catch (error) {
       throw error;
