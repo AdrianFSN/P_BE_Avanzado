5c8c1450478066af5416dd8fc65289fed4555f80
Adrián S <adriansotes@gmail.com>
2024-05-26 18:03:21 +0200

    same

--- routes/tags.js
+++ routes/tags.js
@@ -9,8 +9,6 @@ const UniqueKeysRetriever = require("../utils/UniqueKeysRetriever");
 router.get("/", async function (req, res, next) {
   try {
     const tagsList = await UniqueKeysRetriever.getUniqueKeyValuesFromAPI("tag");
-    //console.log('Esto es tagList', tagsList) // Me va a dar un objeto con results: lista de tags
-    //console.log('Esto es tagList.results', tagsList.results) //Me da ls lista incluida en results
     res.render("tags", { tags: tagsList.results });
   } catch (error) {
     next(error);
